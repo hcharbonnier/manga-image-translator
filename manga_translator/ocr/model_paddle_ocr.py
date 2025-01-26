97a53d272b719f3ab8c35469d898f72bf7302756
your username <yourmail@example.com>
2025-01-27 00:45:54 +0100

    try to add support for paddleOCR

--- manga_translator/ocr/model_paddle_ocr.py
+++ manga_translator/ocr/model_paddle_ocr.py
@@ -64,55 +64,24 @@ class ModelPaddleOCR(OfflineOCR):
 
         quadrilaterals = list(self._generate_text_direction(textlines))
         region_imgs = [q.get_transformed_region(image, d, text_height) for q, d in quadrilaterals]
+        out_regions = []
 
         perm = range(len(region_imgs))
         is_quadrilaterals = False
         if len(quadrilaterals) > 0 and isinstance(quadrilaterals[0][0], Quadrilateral):
-            perm = sorted(range(len(region_imgs)), key = lambda x: region_imgs[x].shape[1])
+            perm = sorted(range(len(region_imgs)), key=lambda x: region_imgs[x].shape[1])
             is_quadrilaterals = True
-        
-        texts = {}
-        merged_idx = [[i] for i in range(len(region_imgs))]
-        merged_quadrilaterals = quadrilaterals
-        merged_region_imgs = []
-        for q, d in merged_quadrilaterals:
-            if d == 'h':
-                merged_text_height = q.aabb.w
-                merged_d = 'h'
-            elif d == 'v':
-                merged_text_height = q.aabb.h
-                merged_d = 'h'
-            merged_region_imgs.append(q.get_transformed_region(image, merged_d, merged_text_height))
-        for idx in range(len(merged_region_imgs)):
-            try:
-                # Use PaddleOCR for OCR
-                result = self.ocr.ocr(merged_region_imgs[idx], cls=True)
-                detected_lang = 'en'  # PaddleOCR does not support language detection
-                if self.logger:
-                    self.logger.info(f"Detected language: {detected_lang}")
-                    self.logger.info(f"OCR result: {result}")  # Log the OCR result
-                for line in result:
-                    print(line)
 
-                texts[idx] = " ".join([line[1][0] for line in result])
-            except Exception as e:
-                if self.logger:
-                    self.logger.error(f"Error during OCR: {e}")
-                texts[idx] = ""
-            
         ix = 0
-        out_regions = {}
         for indices in chunks(perm, max_chunk_size):
             N = len(indices)
             widths = [region_imgs[i].shape[1] for i in indices]
             max_width = 4 * (max(widths) + 7) // 4
-            region = np.zeros((N, text_height, max_width, 3), dtype = np.uint8)
-            idx_keys = []
+            region = np.zeros((N, text_height, max_width, 3), dtype=np.uint8)
             for i, idx in enumerate(indices):
-                idx_keys.append(idx)
                 W = region_imgs[idx].shape[1]
                 tmp = region_imgs[idx]
-                region[i, :, : W, :]=tmp
+                region[i, :, :W, :] = tmp
                 if verbose:
                     os.makedirs('result/ocrs/', exist_ok=True)
                     if quadrilaterals[idx][1] == 'v':
@@ -120,60 +89,39 @@ class ModelPaddleOCR(OfflineOCR):
                     else:
                         cv2.imwrite(f'result/ocrs/{ix}.png', cv2.cvtColor(region[i, :, :, :], cv2.COLOR_RGB2BGR))
                 ix += 1
+
             for i in range(N):
+                try:
+                    # Use PaddleOCR for OCR
+                    result = self.ocr.ocr(region[i], cls=True)
+                    detected_lang = 'en'  # PaddleOCR does not support language detection
+                    if self.logger:
+                        self.logger.info(f"Detected language: {detected_lang}")
+                        self.logger.info(f"OCR result: {result}")  # Log the OCR result
+                    txt = " ".join([line[1][0] for line in result])
+                    prob = 1.0  # Set a default probability
+                except Exception as e:
+                    if self.logger:
+                        self.logger.error(f"Error during OCR: {e}")
+                    txt = ""
+                    prob = 0.0
+
                 cur_region = quadrilaterals[indices[i]][0]
-                cur_region.text = texts[indices[i]]
-                cur_region.prob = 1.0  # Set a default probability
-                out_regions[idx_keys[i]] = cur_region
-                
-        output_regions = []
-        for i, nodes in enumerate(merged_idx):
-            total_logprobs = 0
-            fg_r = []
-            fg_g = []
-            fg_b = []
-            bg_r = []
-            bg_g = []
-            bg_b = []
-            
-            for idx in nodes:
-                if idx not in out_regions:
-                    continue
-                    
-                total_logprobs += np.log(out_regions[idx].prob)
-                fg_r.append(out_regions[idx].fg_r)
-                fg_g.append(out_regions[idx].fg_g)
-                fg_b.append(out_regions[idx].fg_b)
-                bg_r.append(out_regions[idx].bg_r)
-                bg_g.append(out_regions[idx].bg_g)
-                bg_b.append(out_regions[idx].bg_b)
-                
-            prob = np.exp(total_logprobs / len(nodes))
-            fr = round(np.mean(fg_r))
-            fg = round(np.mean(fg_g))
-            fb = round(np.mean(fg_b))
-            br = round(np.mean(bg_r))
-            bg = round(np.mean(bg_g))
-            bb = round(np.mean(bg_b))
-            
-            txt = texts[nodes[0]]  # Ensure correct indexing
-            if self.logger:
-                self.logger.info(f'prob: {prob} {txt} fg: ({fr}, {fg}, {fb}) bg: ({br}, {bg}, {bb})')
-            cur_region = merged_quadrilaterals[i][0]
-            if isinstance(cur_region, Quadrilateral):
-                cur_region.text = txt
-                cur_region.prob = prob
-                cur_region.fg_r = fr
-                cur_region.fg_g = fg
-                cur_region.fg_b = fb
-                cur_region.bg_r = br
-                cur_region.bg_g = bg
-                cur_region.bg_b = bb
-            else: # TextBlock
-                cur_region.text.append(txt)
-                cur_region.update_font_colors(np.array([fr, fg, fb]), np.array([br, bg, bb]))
-            output_regions.append(cur_region)
+                if isinstance(cur_region, Quadrilateral):
+                    cur_region.text = txt
+                    cur_region.prob = prob
+                    cur_region.fg_r = 0
+                    cur_region.fg_g = 0
+                    cur_region.fg_b = 0
+                    cur_region.bg_r = 255
+                    cur_region.bg_g = 255
+                    cur_region.bg_b = 255
+                else:
+                    cur_region.text.append(txt)
+                    cur_region.update_font_colors(np.array([0, 0, 0]), np.array([255, 255, 255]))
+
+                out_regions.append(cur_region)
 
         if is_quadrilaterals:
-            return output_regions
+            return out_regions
         return textlines
